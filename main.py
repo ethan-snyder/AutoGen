a866307046a48f8e242e094bf487bee2451a6367
Ethan Snyder <ems7100@psu.edu>
2025-04-27 23:31:17 -0400

    Base code created, need to copy to clipboard automatically.

---
+++ main.py
@@ -0,0 +1,25 @@
+import random
+import string
+
+# Random letter
+letter = random.choice(string.ascii_letters)  # picks a random letter (uppercase or lowercase)
+symbol = random.choice(string.punctuation)    # picks a random punctuation symbol
+number_char = random.choice(string.digits)    # picks a random digit ('0'-'9')
+password = ''
+
+
+# Main function iterates 20 times and chooses if the character is a num/symbol/digit. Then the proper charac
+#ter type is chosen at random
+for i in range(20):
+    # Random num for choosing weather character will be letter/symbol/num
+    num_char_chooser = random.choice('012')
+    if(num_char_chooser == '0'):
+        curr_char = random.choice(string.ascii_letters)  # picks a random letter (uppercase or lowercase)
+    elif(num_char_chooser == '1'):
+        curr_char = random.choice(string.punctuation)
+    elif(num_char_chooser == '2'):
+        curr_char = random.choice(string.digits)
+    password += curr_char
+
+
+print(password)
